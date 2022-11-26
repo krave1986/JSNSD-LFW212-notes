4050983e14e00d02ae982db17eb36b67ecf45637
Krave <krave@qq.com>
2022-11-26 12:23:26 +0000

    Ch5 lab1 fastify async with promisify done

--- JSNSD(LFW212)/labs/ch-5/labs-1/fastify/routes/boat/index.js
+++ JSNSD(LFW212)/labs/ch-5/labs-1/fastify/routes/boat/index.js
@@ -1,19 +1,30 @@
 'use strict'
 
 const { boat } = require('../../model')
+const { promisify } = require('util')
+const promisifiedRead = promisify(boat.read)
 
 module.exports = async (fastify, opts) => {
     fastify.get('/:id', async (request, reply) => {
-        boat.read(request.params.id, (err, result) => {
-            if (err) {
-                if (err.message === 'not found') {
-                    reply.notFound()
-                    return
-                }
-                reply.send(err)
+        // boat.read(request.params.id, (err, result) => {
+        //     if (err) {
+        //         if (err.message === 'not found') {
+        //             reply.notFound()
+        //             return
+        //         }
+        //         reply.send(err)
+        //     }
+        //     reply.send(result)
+        // })
+        // await reply
+        try {
+            return await promisifiedRead(request.params.id)
+        } catch (err) {
+            if (err.message === 'not found') {
+                reply.notFound()
+                return
             }
-            reply.send(result)
-        })
-        await reply
+            throw err
+        }
     })
 }
