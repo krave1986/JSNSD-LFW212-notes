d7c7024249c23232824419b183eb89d10a308ceb
Krave <krave@qq.com>
2022-11-28 23:00:25 +0000

    fastify finished

--- JSNSD(LFW212)/Status codes used in the course.md
+++ JSNSD(LFW212)/Status codes used in the course.md
@@ -5,3 +5,4 @@ Code | Description
 200 | OK
 201 | Created
 204 | No Content *(This is the only case for JSON service without `Content-Type` being set)*
+404 | Not found *This error offten created by libs such as `fastify.httpErrors.notFound()`*
--- JSNSD(LFW212)/course/Chapter 5/my-service/routes/bicycle/index.js
+++ JSNSD(LFW212)/course/Chapter 5/my-service/routes/bicycle/index.js
@@ -7,6 +7,7 @@ const { uid } = bicycle
 const read = promisify(bicycle.read)
 const create = promisify(bicycle.create)
 const update = promisify(bicycle.update)
+const del = promisify(bicycle.del)
 
 module.exports = async (fastify, opts) => {
     const { notFound } = fastify.httpErrors
@@ -43,5 +44,36 @@ module.exports = async (fastify, opts) => {
             }
             throw err
         }
+    })
+
+    fastify.put('/:id', async (request, reply) => {
+        const { id } = request.params
+        const { data } = request.body
+        try {
+            await create(id, data)
+            reply.code(201)
+            return {}
+        } catch (err) {
+            if (err.message === 'resource exists') {
+                await update(id, data)
+                reply.code(204)
+            } else {
+                throw err
+            }
+        }
+    })
+
+    fastify.delete('/:id', async (request, reply) => {
+        const { id } = request.params
+        try {
+            await del(id)
+            reply.code(204)
+        } catch (err) {
+            if (err.message === 'not found') {
+                throw notFound()
+            }
+            throw err
+        }
+        request, reply
     })
 }
