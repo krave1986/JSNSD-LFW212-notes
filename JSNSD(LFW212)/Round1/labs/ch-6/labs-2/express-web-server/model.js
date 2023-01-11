3a1926476a83f8d5f2114afea3bc297b7b5aa554
Krave <krave@qq.com>
2023-01-11 00:09:28 +0000

    Round1 lab6.2 express done

--- JSNSD(LFW212)/Round1/labs/ch-6/labs-2/express-web-server/model.js
+++ JSNSD(LFW212)/Round1/labs/ch-6/labs-2/express-web-server/model.js
@@ -59,10 +59,7 @@
     }
   
     function del (id, cb) {
-      if (id === '821e') {
-        setImmediate(() => cb(Error('unknown')))
-        return
-      }
+
       if (!(db.hasOwnProperty(id))) {
         const err = Error('not found')
         err.code = 'E_NOT_FOUND'
---
+++ JSNSD(LFW212)/Round1/labs/ch-6/labs-2/express-web-server/routes/boat.js
@@ -0,0 +1,58 @@
+const { Router } = require('express')
+
+const { boat } = require('../model')
+
+const createError = require("http-errors")
+
+const router = Router()
+
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params
+    boat.read(id, (err, theBoat) => {
+        if (err) {
+            if (err.message === 'not found') {
+                err.status = 404
+            }
+            next(err)
+        } else {
+            res.type('application/json')
+            res.end(JSON.stringify(theBoat))
+        }
+    })
+})
+
+router.post('/', (req, res, next) => {
+    const isJSON = req.headers['content-type'] === 'application/json'
+    if (isJSON) {
+        const { data } = req.body
+        const uid = boat.uid()
+        boat.create(uid, data, (err) => {
+            if (err) {
+                next(err)
+            } else {
+                res.status(201)
+                res.send({ id: uid })
+            }
+        })
+    } else {
+        next(Error('wrong type'))
+    }
+})
+
+router.delete('/:id', (req, res, next) => {
+    const { id } = req.params
+    boat.del(id, (err) => {
+        if (err) {
+            if (err.message === 'not found') {
+                next(createError(404))
+            } else {
+                next(err)
+            }
+        } else {
+            res.status(204)
+            res.end()
+        }
+    })
+})
+
+module.exports = router
