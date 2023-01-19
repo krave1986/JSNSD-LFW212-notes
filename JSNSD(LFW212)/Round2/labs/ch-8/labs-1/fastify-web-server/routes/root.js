3b14448c882c85136a4b8cd83d23dc60f3583223
Krave <krave@qq.com>
2023-01-19 00:38:10 +0000

    Round2 lab8.1 fastify done

---
+++ JSNSD(LFW212)/Round2/labs/ch-8/labs-1/fastify-web-server/routes/root.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    const { url } = request.query
+    try {
+      new URL(url)
+    } catch (err) {
+      return reply.badRequest()
+    }
+    return reply.from(url)
+  })
+}
