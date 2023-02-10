661cb6caca55752372d23e87c22b7345e1b92ca8
Krave <krave@qq.com>
2023-02-10 23:05:17 +0000

    Round 3 lab 6.2 fastify done

---
+++ JSNSD(LFW212)/Round3/labs/ch-6/labs-2/fastify-web-server/routes/boat/index.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const { promisify } = require('util')
+
+
+const { boat } = require('../../model')
+
+const read = promisify(boat.read)
+const create = promisify(boat.create)
+const del = promisify(boat.del)
+
+
+module.exports = async (fastify, opts) => {
+    fastify.get('/:id', async (request, reply) => {
+        const { id } = request.params
+
+        try {
+            const theBoat = await read(id)
+            return theBoat
+        } catch (err) {
+            if (err.message === 'not found') {
+                return reply.notFound()
+            }
+            reply.send(err)
+        }
+    })
+
+    fastify.post('/', async (request, reply) => {
+        const isJSON = request.headers['content-type'] === 'application/json'
+        if (isJSON) {
+            try {
+                const id = boat.uid()
+                await create(id, request.body.data)
+                reply.code(201)
+                return { id }
+            } catch (err) {
+                throw err
+            }
+        } else {
+            return reply.badRequest()
+        }
+    })
+
+    fastify.delete('/:id', async (request, reply) => {
+        const { id } = request.params
+        try {
+            await del(id)
+            reply.code(204)
+            return
+        } catch (err) {
+            if (err.message === 'not found') {
+                return reply.notFound()
+            }
+            throw err
+        }
+    })
+}
